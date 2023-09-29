e3215d747bd0a4ce3555033cab88772f6026638b
Wolfgang Kinkeldei <wkinkeldei@outlook.de>
2023-09-29 18:16:20 +0200

    first simple try

--- src/ActorSystem/actor.ts
+++ src/ActorSystem/actor.ts
@@ -4,11 +4,11 @@ import { IMessage } from './message';
 import { ActorState } from './actor-state';
 
 export abstract class Actor {
-    private processor?: NodeJS.Timeout = undefined;
+    private processor?: NodeJS.Immediate = undefined; // NodeJS.Timeout = undefined;
     private state: ActorState = ActorState.Idle;
     private messages: Envelope[] = [];
     protected currentlyProcessing?: Envelope;
-    protected sender: Actor = NullActor.Instance;
+    protected sender: Actor = this;
 
     constructor() {
     }
@@ -18,11 +18,11 @@ export abstract class Actor {
     }
 
     protected reply(message: IMessage) {
-        this.sender.addEnvelope({sender: this, receiver: this.sender, message})
+        this.sender!.addEnvelope({sender: this, receiver: this.sender!, message})
     }
 
     protected forward(receiver: Actor) {
-        receiver.addEnvelope({sender: this.sender, receiver, message: this.currentlyProcessing!.message});
+        receiver.addEnvelope({sender: this.sender!, receiver, message: this.currentlyProcessing!.message});
     }
 
     private addEnvelope(envelope: Envelope) {
@@ -32,46 +32,44 @@ export abstract class Actor {
 
     private ensureMessageProcessing() {
         if (this.messages.length > 0 && this.processor == undefined && this.state === ActorState.Idle) {
-            this.processor = setTimeout(this.processMessage.bind(this), 0);
+            this.processor = setImmediate(this.processMessage.bind(this));
         }
     }
 
     private async processMessage() {
         this.processor = undefined;
+        if (this.messages.length == 0) return;
 
-        if (this.messages.length > 0) {
-            this.state = ActorState.Processing;
-            this.currentlyProcessing = this.messages.shift();
-            this.sender = this.currentlyProcessing!.sender;
+        this.state = ActorState.Processing;
+        this.currentlyProcessing = this.messages.shift();
+        this.sender = this.currentlyProcessing!.sender;
 
-            try {
-                await this.onReceive(this.currentlyProcessing!.message);
-            }
-            catch (e) {
-                this.state = ActorState.ErrorHandling;
-                // TODO: call OnError Hook
-                // decide if restart possible (maybe delay)
-                // TODO: call Restart Hook
-            }
-
-            this.state = ActorState.Idle;
-            this.currentlyProcessing = undefined;
-            this.sender = NullActor.Instance;
-
-            this.ensureMessageProcessing();
+        try {
+            await this.onReceive(this.currentlyProcessing!.message);
+        } catch (e) {
+            this.state = ActorState.ErrorHandling;
+            // TODO: call OnError Hook
+            // decide if restart possible (maybe delay)
+            // TODO: call Restart Hook
         }
+
+        this.state = ActorState.Idle;
+        this.currentlyProcessing = undefined;
+        this.sender = this;
+
+        this.ensureMessageProcessing();
     }
 
     protected abstract onReceive(message: any): Promise<void>;
 }
 
-export class NullActor extends Actor {
-    public static Instance: NullActor;
-
-    protected override async onReceive(message: any): Promise<void> {
-        await (async () => {})();
-    }
-}
-
-NullActor.Instance ??= new NullActor();
+// export class NullActor extends Actor {
+//     public static Instance: NullActor;
+//
+//     protected override async onReceive(message: any): Promise<void> {
+//         await (async () => {})();
+//     }
+// }
+//
+// NullActor.Instance ??= new NullActor();
 
--- src/index.ts
+++ src/index.ts
@@ -11,30 +11,58 @@ interface Pong extends IMessage {
     text: string
 }
 
+interface Benchmark extends IMessage {
+    type: 'Benchmark',
+    count?: number
+}
+
+interface Finish extends IMessage {
+    type: 'Finish',
+    durationMs: number
+}
+
+type MyMessage = Ping | Pong
+
+const BENCHMARK_COUNT = 20_000;
+
 class Sender extends Actor {
+    private start: number = 0;
+
     constructor(private receiver: Actor) {
         super();
-        let x = {type: 'Ping', text: 'hi'};
         this.tell(receiver, <Ping>{type: 'Ping', text: 'hi'});
     }
 
     protected override async onReceive(message: IMessage): Promise<void> {
-        console.log('Sender received: ', message);
+        // console.log('Sender received: ', message);
+
+        if (message.type === 'Benchmark') {
+            console.log('benchmarking...');
+            this.start = Date.now();
+            for (var i= 1; i <= BENCHMARK_COUNT; i++)
+                this.tell(receiver, <Benchmark>{type: 'Benchmark', count: i});
+            console.log('benchmark sending done');
+        } else if (message.type === 'Finish') {
+            let duration = Date.now() - this.start;
+            let thruput = parseInt('' + BENCHMARK_COUNT/(duration/1000));
+            console.log(`Sending ${BENCHMARK_COUNT} messages took ${duration}ms (${thruput} msgs/s)`);
+        } else if (message.type === 'Pong') {
+            console.log('Sender received Pong');
+        }
 
         await (async () => {})();
     }
 }
 
 class Receiver extends Actor {
-    constructor() {
-        super();
-    }
-
     protected override async onReceive(message: IMessage): Promise<void> {
-        console.log('Receiver received: ', message);
-
         if (message.type === 'Ping') {
+            console.log('Receiver received: ', message);
             this.reply(<Pong>{type: 'Pong', text: 'also hi'});
+        } else if (message.type === 'Benchmark') {
+            if ((<Benchmark>message).count === BENCHMARK_COUNT) {
+                this.reply(<Finish>{type: 'Finish'});
+            }
         }
 
         await (async () => {})();
@@ -46,4 +74,8 @@ console.log('About to start');
 let receiver = new Receiver();
 let sender = new Sender(receiver);
 
-setTimeout(() => {console.log('All Done')}, 2000);
+// start Benchmark after 500ms
+setTimeout(() => {sender.tell(sender, <Benchmark>{type: 'Benchmark'})}, 500);
+
+// keep system running for 10 seconds
+setTimeout(() => {console.log('All Done')}, 10_000);
